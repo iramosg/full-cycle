e136b2c0517ed1b5d6bb629cf70c4ef69efe09ef
Igor Ramos <igor.ramos@live.com>
2023-02-12 20:53:35 -0300

    Trabalhando com streams bidirecionais

--- comunicacao-entre-sistemas/grpc/internal/service/category.go
+++ comunicacao-entre-sistemas/grpc/internal/service/category.go
@@ -88,11 +88,40 @@ func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateC
 		if err != nil {
 			return err
 		}
-		
+
 		categories.Categories = append(categories.Categories, &pb.Category{
 			Id:          categoryResult.ID,
 			Name:        categoryResult.Name,
 			Description: categoryResult.Description,
 		})
+	}
+}
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		err = stream.Send(&pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+		
+		if err != nil {
+			return err
+		}
 	}
 }
